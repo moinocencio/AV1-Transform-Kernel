af5e9a2abe5850f304724a13cbd9dddaf82d2cfc
moinocencio <moinocencio@gmail.com>
2019-10-27 23:43:04 +0000

    Bash Script for transform tests

---
+++ Developed_Work/Libaom/Tests/testAllTrans.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+frames=10
+
+## Encode
+## CIF: waterfall
+./testTrans.sh -r 1 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/CIF/waterfall_cif.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/CIF/waterfall_ttest -l $frames
+
+## CIF: flower
+./testTrans.sh -r 1 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/CIF/flower_cif.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/CIF/flower_ttest -l $frames
+
+## CIF: bridge
+./testTrans.sh -r 1 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/CIF/bridge_close_cif.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/CIF/bridge_ttest -l $frames
+
+## HD: parkrun
+./testTrans.sh -r 2 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/HD/720p50_parkrun_ter.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/HD/parkrun_ttest -l $frames
+
+## HD: Ducks
+./testTrans.sh -r 2 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/HD/ducks_take_off_420_720p50.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/HD/ducks_ttest -l $frames
+
+## HD: Shields
+./testTrans.sh -r 2 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/HD/720p50_shields_ter.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/HD/shields_ttest -l $frames
+
+## FHD: Dinner
+./testTrans.sh -r 3 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/FHD/dinner_1080p30.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/FHD/dinner_ttest -l $frames
+
+## FHD: Factory
+./testTrans.sh -r 3 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/FHD/factory_1080p30.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/FHD/factory_ttest -l $frames
+
+## FHD: Park Joy
+./testTrans.sh -r 3 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/FHD/park_joy_1080p50.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/FHD/parkjoy_ttest -l $frames
+
+## UHD: Old town Cross
+./testTrans.sh -r 4 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/UHD/old_town_cross_2160p50.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/UHD/oldtowncross_ttest -l $frames
+
+## UHD: Into tree
+./testTrans.sh -r 4 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/UHD/in_to_tree_2160p50.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/UHD/intotree_ttest -l $frames
+
+## UHD: Crowd Run
+./testTrans.sh -r 4 -i /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Test_Videos/UHD/crowd_run_2160p50.y4m -o /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Enc_DecVideos/UHD/crowdrun_ttest -l $frames
+
--- Developed_Work/Libaom/Tests/testTrans.sh
+++ Developed_Work/Libaom/Tests/testTrans.sh
@@ -58,18 +58,28 @@ while getopts ':he:i:r:l:q:o:' option; do
 done
 shift "$((OPTIND - 1))"
 
-echo -e "\n\e[1m\e[31mRegular Transform Encoding\e[0m"
-./regenc.sh -i $inpath -r $res -o $outpath"_reg_trans.webm" -q $qual -l $limit
-/run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Builds/Regular/aomdec $outpath"_reg_trans.webm" --i420 -o $outpath"_reg_trans.y4m"
+echo -e "\n\e[1m\e[31mEncoding Test: $inpath\n\e[1m\e[31mRegular Transform Encoding\e[0m" 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+echo -e "\n\e[1mHigh Quality\e[0m\n" 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+./regenc.sh -i $inpath -r $res -o $outpath"_reg_trans5.webm" -q 5 -l $limit 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+/run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Builds/Regular/aomdec $outpath"_reg_trans5.webm" --i420 -o $outpath"_reg_trans5.y4m"
 
-echo -e "\n\e[1m\e[32mMultiplier Transform Encoding\e[0m"
-./regenc.sh -e $transencmult -i $inpath -r $res -o $outpath"_mult_trans.webm" -q $qual -l $limit
-/run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Builds/Regular/aomdec $outpath"_mult_trans.webm" --i420 -o $outpath"_mult_trans.y4m"
+echo -e "\n\e[1mMedium Quality\e[0m\n" 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+./regenc.sh -i $inpath -r $res -o $outpath"_reg_trans25.webm" -q 25 -l $limit 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+/run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Builds/Regular/aomdec $outpath"_reg_trans25.webm" --i420 -o $outpath"_reg_trans25.y4m"
 
-echo -e "\n\e[1m\e[33mShift Transform Encoding\e[0m"
-./regenc.sh -e $transencshift -i $inpath -r $res -o $outpath"_shift_trans.webm" -q $qual -l $limit
-/run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Builds/Regular/aomdec $outpath"_shift_trans.webm" --i420 -o $outpath"_shift_trans.y4m"
+echo -e "\n\e[1mLow Quality\e[0m\n" 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+./regenc.sh -i $inpath -r $res -o $outpath"_reg_trans60.webm" -q 60 -l $limit 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+/run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Builds/Regular/aomdec $outpath"_reg_trans60.webm" --i420 -o $outpath"_reg_trans60.y4m"
 
-echo -e "\n\e[1m\e[34mButterfly Multiplication Transform Encoding\e[0m"
-./regenc.sh -e $transencbuttmult -i $inpath -r $res -o $outpath"_buttmult_trans.webm" -q $qual -l $limit
-/run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Builds/Regular/aomdec $outpath"_buttmult_trans.webm" --i420 -o $outpath"_buttmult_trans.y4m"
+echo -e "\n\e[1m\e[34mButterfly Multiplication Transform Encoding\e[0m" 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+echo -e "\n\e[1mHigh Quality\e[0m\n" 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+./regenc.sh -e $transencbuttmult -i $inpath -r $res -o $outpath"_butt_trans5.webm" -q 5 -l $limit 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+/run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Builds/Regular/aomdec $outpath"_butt_trans5.webm" --i420 -o $outpath"_butt_trans5.y4m"
+
+echo -e "\n\e[1mMedium Quality\e[0m\n" 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+./regenc.sh -e $transencbuttmult -i $inpath -r $res -o $outpath"_butt_trans25.webm" -q 25 -l $limit 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+/run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Builds/Regular/aomdec $outpath"_butt_trans25.webm" --i420 -o $outpath"_butt_trans25.y4m"
+
+echo -e "\n\e[1mLow Quality\e[0m\n" 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+./regenc.sh -e $transencbuttmult -i $inpath -r $res -o $outpath"_butt_trans60.webm" -q 60 -l $limit 2>&1 | tee -a /run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Tests/Encode_Stats/TestTransLog.txt
+/run/media/moinocencio/Data/Tese/Master-Thesis/Developed_Work/Libaom/Builds/Regular/aomdec $outpath"_butt_trans60.webm" --i420 -o $outpath"_butt_trans60.y4m"
